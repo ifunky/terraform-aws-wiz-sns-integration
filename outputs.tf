9b1caf0b9a802369b58ca02fff7e9b1322a6736d
Dan Gibbons <dan.gibbons@wiz.io>
2022-10-02 20:56:23 +0100

    Add TF descriptions and types

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "sns_topic" {
-  value = aws_sns_topic.wiz.arn
+  value       = aws_sns_topic.wiz.arn
+  description = "ARN of the SNS topic created"
 }
--- variables.tf
+++ variables.tf
@@ -5,18 +5,22 @@ variable "tags" {
 }
 
 variable "external_id" {
+    type        = string
     description = "Wiz `external id`"
 }
 
 variable "lambda_target_arn" {
+    type        = string
     description = "SNS Subscription Lambda target ARN"
 }
 
 variable "topic_name" {
+    type        = string
     description = "SNS topic name"
 }
 
 variable "wiz_account_id" {
+    type        = string
     description = "Wiz account ID that is allowed to assume the role"
     default     = "197171649850"
 }
